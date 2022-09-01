b997b23d6cfde61aca1eb5361b2610b699d8d5fe
Ira Korytko <irichkakorytko@yandex.ru>
2022-09-01 12:42:04 +0300

    урок 11

--- script.js
+++ script.js
@@ -2,20 +2,31 @@
 const btn = document.getElementById("btn");
 const text = document.getElementById("text");
 
-const button = document.querySelector('#e_btn')
-const circle = document.querySelector('#circle')
-const squere = document.querySelector('#square')
-
+// const button = document.querySelector('#e_btn')
+// const circle = document.querySelector('#circle')
+const square = document.querySelector('square')
 
 const range = document.getElementById("range");
 const span = document.getElementById("range-span");
+const circle = document.getElementById('circle');
+
+
+const eBtn = document.getElementById('e_btn');
+eBtn.style="display: none;" //пункт 2 домашки
+
+
+
 
 const logger = function (event) {
   console.log(event.type);
   span.textContent = event.target.value
 }
 range.addEventListener('input', logger);
-range.addEventListener('change', logger);
+range.addEventListener('change', logger); //пункт 3 домашки
+
+range.textContent = range.value + "%";
+range.style.width = range.value + "%";
+
 
 
 button.onclick = function () {
@@ -32,7 +43,3 @@ button.addEventListener('click', toggler);
 circle.addEventListener('click', toggler);
 square.addEventListener('click', toggler);
 
-
-// btn.addEventListener('click', function () {
-  
-// })
